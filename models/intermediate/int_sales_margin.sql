ae2a98729d42961881d667aa0acc484c42154ace
elias.lehmkuhler@gmail.com <elias.lehmkuhler@gmail.com>
2024-10-29 16:55:42 +0000

    ofgäoö

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -3,3 +3,7 @@ SELECT
 FROM {{ ref('stg_raw__product') }}
 JOIN {{ ref('stg_raw__sales') }}
 ON stg_raw__product.products_id = stg_raw__sales.pdt_id
+
+SELECT
+*
+FROM {{ ref('int_sales_margin') }}
--- models/mart/finance_days.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-date_date
-,SUM(orders_id) as nb_transaction 
-,SUM(revenue) as total_revenue
-,SUM(quantity)/SUM(orders_id) as average_basket
-,Quantity* purchse_price as purchase_cost
-FROM {{ ref('ru') }}
