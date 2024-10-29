b1e7a316c5a28d57f47cd5aab4ac2f92da5c9a15
elias.lehmkuhler@gmail.com <elias.lehmkuhler@gmail.com>
2024-10-29 17:05:37 +0000

    klwö

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,7 +1,5 @@
 SELECT
-stg_raw__sales.*
-,CAST(purchse_price as INT64) as purchse_price
-,products_id
+*
 FROM {{ ref('stg_raw__product') }}
 JOIN {{ ref('stg_raw__sales') }}
 ON stg_raw__sales.pdt_id = stg_raw__product.products_id
