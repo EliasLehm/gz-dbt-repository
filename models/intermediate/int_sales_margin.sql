15a447eba695f81687e0800a9efbae9b4b02d953
elias.lehmkuhler@gmail.com <elias.lehmkuhler@gmail.com>
2024-10-29 16:50:03 +0000

    pr

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,5 @@
+SELECT
+*
+FROM {{ ref('stg_raw__product') }}
+JOIN {{ ref('stg_raw__sales') }}
+ON stg_raw__product.products_id = stg_raw__sales.pdt_id
--- models/intermediate/ru.sql
+++
@@ -1,9 +0,0 @@
-select
-*
-FROM {{ ref('stg_raw__product') }}
-JOIN {{ ref('stg_raw__sales') }}
-ON stg_raw__product.products_id = stg_raw__sales.pdt_id;
-
-SELECT
-*
-FROM {{ ref('ru') }}
