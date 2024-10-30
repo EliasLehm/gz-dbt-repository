bd556432c04c3373c3c50cefcfe97fe5906526f0
elias.lehmkuhler@gmail.com <elias.lehmkuhler@gmail.com>
2024-10-30 10:35:51 +0000

    iuföp

--- models/mart/gu.sql
+++ models/mart/gu.sql
@@ -1,12 +1,14 @@
+
+
 SELECT
 date_date
-,SUM(nb_transaction) as nb_transaction
-,SUM (revenue) as revenue
-,AVG(average_basket) average_basket
+,COUNT(nb_transaction) as nb_transaction
+,SUM(revenue) as revenue
+,AVG(average_basket) as average_basket
 ,SUM(revenue-purchase_cost-logcost-ship_cost) as operational_margin
 ,SUM(logcost) as logcost
 ,SUM (shipping_fee) as shipping_fee
-,SUM(Total_products) as Total_products
+,COUNT(Total_products) as Total_products
 ,SUM(ship_cost) as ship_cost
 FROM {{ ref('new') }}
 GROUP BY date_date
